ccd63948a16c76e718c049dabd1c74c2ced3aa27
Junho Park <ghdlwnsgh12@naver.com>
2026-02-16 15:50:52 +0000

    chore: remove apps/frontend

--- apps/frontend/prisma/migrations/20260212131637_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title]` on the table `differentiators` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "differentiators_title_key" ON "differentiators"("title");
